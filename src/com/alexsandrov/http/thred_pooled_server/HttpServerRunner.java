95bca31cf52b33a6ab55afe7dea833aecf478ce0
Morrrun <mifon.199708@gmail.com>
2023-04-28 15:18:18 +0400

    Произведен рефакторинг Клиента для отправки асинхронных запросов Тестовый запуск клиента

--- src/com/alexsandrov/http/thred_pooled_server/HttpServerRunner.java
+++ src/com/alexsandrov/http/thred_pooled_server/HttpServerRunner.java
@@ -4,7 +4,7 @@ import com.alexsandrov.http.thred_pooled_server.server.HttpServer;
 
 public class HttpServerRunner {
     public static void main(String[] args) {
-//        HttpServer httpServer = new HttpServer(9000);
-//        httpServer.run();
+        HttpServer httpServer = new HttpServer(9000, 100);
+        httpServer.run();
     }
 }
